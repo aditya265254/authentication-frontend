835e82720094490dd642fa6deaab8489c5781ec8
Aditya <akash.aditya001@gmail.com>
2026-07-04 12:33:56 +0530

    Add user data storage on successful login and enhance dashboard with user greeting

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
-export default Dashboard
+  const handleLogout = () => {
+    localStorage.removeItem("token")
+    localStorage.removeItem("user")
+    navigate('/')
+  }
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+      navigate("/");
+    }
+
+    const user = JSON.parse(localStorage.getItem("user"));
+    setUser(user);
+  }, []);
+
+  return (
+    <div>
+        <h1>Welcome, {user?.fullName}</h1>
+        <p>{user?.email}</p>
+        <button onClick={handleLogout}>Logout</button>
+    </div>
+)
+};
+
+export default Dashboard;
